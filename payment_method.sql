68ae836bf05eb16c4f1fd11451fe6a6a16dca1b1
Rose-Zahra-Mohammadi <mohamadi.z.2016@gmail.com>
2023-12-06 10:34:29 -0500

    sql inserts

---
+++ payment_method.sql
@@ -0,0 +1,4 @@
+INSERT INTO payment_method (card_type, card_number)
+VALUES ('Debit', '1111222233334444');
+INSERT INTO payment_method (card_type, card_number)
+VALUES ('Credit', '1111222233335555');
