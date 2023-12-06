1018f0ca2b51176ec88f43be3922ba23bf90612d
Rose-Zahra-Mohammadi <mohamadi.z.2016@gmail.com>
2023-12-06 12:42:58 -0500

    update order

--- src/main/java/org/example/dao/OrderDao.java
+++ src/main/java/org/example/dao/OrderDao.java
@@ -2,6 +2,7 @@ package org.example.dao;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -41,4 +42,81 @@ public class OrderDao {
     return cd;
   }
 
+  public Order getOrderById(int orderId) {
+    Order order = null;
+    String query = "SELECT * FROM ride_order WHERE id = ?";
+    try (Connection conn = DBConnector.getConnection();
+         PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+      pstmt.setInt(1, orderId);
+      try (ResultSet rs = pstmt.executeQuery()) {
+        if (rs.next()) {
+          order = mapRowToOrder(rs);
+        }
+      }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+      // Handle or log the error appropriately
+    }
+    return order;
+  }
+
+  private Order mapRowToOrder(ResultSet rs) throws SQLException {
+    Order order = new Order();
+    order.setId(rs.getInt("id"));
+    order.setOrderDate(rs.getDate("order_date"));
+    order.setDesiredCapacity(rs.getInt("desired_capacity"));
+    order.setAccessibility(rs.getBoolean("accessibility"));
+    order.setStartCity(rs.getString("start_city"));
+    order.setEndCity(rs.getString("end_city"));
+    // Add other mappings as needed
+    return order;
+  }
+
+  public void updateOrderCapacity(int orderId, int newCapacity) {
+    String query = "UPDATE ride_order SET desired_capacity = ? WHERE id = ?";
+    try (Connection conn = DBConnector.getConnection();
+         PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+      pstmt.setInt(1, newCapacity);
+      pstmt.setInt(2, orderId);
+      pstmt.executeUpdate();
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+      // Handle or log the error appropriately
+    }
+  }
+
+  public void updateOrderAccessibility(int orderId, boolean newAccessibility) {
+    String query = "UPDATE ride_order SET accessibility = ? WHERE id = ?";
+    try (Connection conn = DBConnector.getConnection();
+         PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+      pstmt.setBoolean(1, newAccessibility);
+      pstmt.setInt(2, orderId);
+      pstmt.executeUpdate();
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+      // Handle or log the error appropriately
+    }
+  }
+
+  public void updateOrderRoute(int orderId, String newStartCity, String newEndCity) {
+    String query = "UPDATE ride_order SET start_city = ?, end_city = ? WHERE id = ?";
+    try (Connection conn = DBConnector.getConnection();
+         PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+      pstmt.setString(1, newStartCity);
+      pstmt.setString(2, newEndCity);
+      pstmt.setInt(3, orderId);
+      pstmt.executeUpdate();
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+      // Handle or log the error appropriately
+    }
+  }
 }
